31f8da31e115da9f364aed4fd45f0df69eca185d
danielMouimi <mouimidaniel@gmail.com>
2025-06-16 13:47:47 +0200

    deploy front

--- config/cors.php
+++ config/cors.php
@@ -1,17 +1,17 @@
 <?php
 return [
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie', '*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://tfgfront-production.up.railway.app'],
 
-    'allowed_origins_patterns' => ['*'],
+    'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
 
-    'exposed_headers' => ['*'],
+    'exposed_headers' => [],
 
     'max_age' => 0,
 
